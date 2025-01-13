4de9eb9ca194d7f7bf2dadf4d210295e2941b2f8
William Franco <williamff@outlook.com>
2025-01-13 17:48:48 -0300

    :white_check_mark: Added feature settings

---
+++ lib/src/features/settings/controllers/setting_controller.dart
@@ -0,0 +1,48 @@
+import 'package:f_launcher/src/features/settings/models/setting_model.dart';
+import 'package:f_launcher/src/features/settings/repositories/setting_repository.dart';
+import 'package:flutter/material.dart';
+
+typedef _Controller = ValueNotifier<SettingModel>;
+
+abstract interface class SettingController extends _Controller {
+  SettingController() : super(SettingModel());
+
+  Future<void> loadTheme();
+  Future<void> changeTheme({required bool isDarkTheme});
+}
+
+class SettingControllerImpl extends _Controller implements SettingController {
+  final SettingRepository settingRepository;
+
+  SettingControllerImpl({
+    required this.settingRepository,
+  }) : super(SettingModel()) {
+    loadTheme();
+  }
+
+  @override
+  Future<void> loadTheme() async {
+    bool isDarkTheme = await settingRepository.readTheme();
+    final settingModel = SettingModel(
+      isDarkTheme: isDarkTheme,
+    );
+    value = settingModel;
+    _debug();
+  }
+
+  @override
+  Future<void> changeTheme({required bool isDarkTheme}) async {
+    await settingRepository.updateTheme(
+      isDarkTheme: isDarkTheme,
+    );
+    final settingModel = SettingModel(
+      isDarkTheme: isDarkTheme,
+    );
+    value = settingModel;
+    _debug();
+  }
+
+  void _debug() {
+    debugPrint('Dark theme: ${value.isDarkTheme}');
+  }
+}
---
+++ lib/src/features/settings/views/setting_view.dart
@@ -0,0 +1,97 @@
+import 'package:f_launcher/src/common/dependency_injectors/dependency_injector.dart';
+import 'package:f_launcher/src/common/enums/launcher_filter_enum.dart';
+import 'package:f_launcher/src/features/launcher/controllers/launcher_controller.dart';
+import 'package:f_launcher/src/features/settings/controllers/setting_controller.dart';
+import 'package:f_launcher/src/features/settings/models/setting_model.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SettingView extends StatefulWidget {
+  const SettingView({super.key});
+
+  @override
+  State<SettingView> createState() => _SettingViewState();
+}
+
+class _SettingViewState extends State<SettingView> {
+  late final SettingController settingController;
+  late final LauncherController launcherController;
+
+  @override
+  void initState() {
+    super.initState();
+    settingController = locator<SettingController>();
+    launcherController = locator<LauncherController>();
+  }
+
+  void _showAboutDialog() {
+    showAboutDialog(
+      context: context,
+      applicationIcon: const FlutterLogo(),
+      applicationName: 'F Launcher',
+      applicationVersion: 'Version 1.0.0',
+      applicationLegalese: '\u{a9} 2024 William Franco',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text('Settings'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_outlined),
+          onPressed: () {
+            context.pop();
+          },
+        ),
+      ),
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: const Text('Select application type'),
+              trailing: DropdownButton<LauncherFilterEnum>(
+                value: launcherController.currentFilter,
+                items: LauncherFilterEnum.values.map((filter) {
+                  return DropdownMenuItem<LauncherFilterEnum>(
+                    value: filter,
+                    child: Text(filter.name),
+                  );
+                }).toList(),
+                onChanged: (filter) {
+                  if (filter != null) {
+                    launcherController.updateFilter(filter);
+                  }
+                },
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.brightness_6_outlined),
+              title: const Text('Dark theme'),
+              trailing: ValueListenableBuilder<SettingModel>(
+                valueListenable: settingController,
+                builder: (context, value, widget) {
+                  return Switch(
+                    value: value.isDarkTheme,
+                    onChanged: (bool isDarkTheme) {
+                      settingController.changeTheme(isDarkTheme: isDarkTheme);
+                    },
+                  );
+                },
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.info_outline),
+              title: const Text('About'),
+              onTap: () {
+                _showAboutDialog();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
